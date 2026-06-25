410938c8b89daf94edf49d5efe57c189dfc9110d
mr-brune <49653622+mr-brune@users.noreply.github.com>
2026-06-25 07:02:36 +0200

    fix: remove tags text block from description, tags are now shown as clickable chips in genre

--- src/all/ehentai/src/eu/kanade/tachiyomi/extension/all/ehentai/MetadataCopier.kt
+++ src/all/ehentai/src/eu/kanade/tachiyomi/extension/all/ehentai/MetadataCopier.kt
@@ -84,19 +84,7 @@ fun ExGalleryMetadata.copyTo(manga: SManga) {
         detailsDesc += "\n"
     }
 
-    val tagsDesc = buildTagsDescription(this)
-
-    manga.description = listOf(titleDesc.toString(), detailsDesc.toString(), tagsDesc.toString())
+    manga.description = listOf(titleDesc.toString(), detailsDesc.toString())
         .filter(String::isNotBlank)
         .joinToString(separator = "\n")
-}
-
-private fun buildTagsDescription(metadata: ExGalleryMetadata) = StringBuilder("Tags:\n").apply {
-    // BiConsumer only available in Java 8, we have to use destructuring here
-    metadata.tags.forEach { (namespace, tags) ->
-        if (tags.isNotEmpty()) {
-            val joinedTags = tags.joinToString(separator = " ", transform = { "<${it.name}>" })
-            this += "▪ $namespace: $joinedTags\n"
-        }
-    }
 }
